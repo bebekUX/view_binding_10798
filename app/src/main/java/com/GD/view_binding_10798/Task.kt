66aab117b1ae8a1b3f4cbe0800b56beccdf23414
bebekUX <briantana.869@gmail.com>
2022-09-15 08:11:31 +0700

    GD 4 View Binding 0798

---
+++ app/src/main/java/com/GD/view_binding_10798/Task.kt
@@ -0,0 +1,3 @@
+package com.GD.view_binding_10798
+
+data class Task (val chapter:String, val title:String,val details:String)
---
+++ app/src/main/java/com/GD/view_binding_10798/TaskList.kt
@@ -0,0 +1,11 @@
+package com.GD.view_binding_10798
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1", "Bangun Tidur","Hari semakin indah"),
+        Task("Chapter 2", "Minum Aqua","Biar Melek"),
+        Task("Chapter 3", "Login PUBG","BY ONE OM"),
+        Task("Chapter 4", "RIDING ","P Balap?"),
+        Task("Chapter 5", "BOBOK","Biar seger")
+    )
+}
